7a80c64e79ad7664e5953ba83ca51588ad2658c9
Margaret Black <margaret.carlin93@hotmail.co.uk>
2024-01-06 22:04:35 +0000

    fix: Implement functionality for health and readiness endpoints

--- activity-tracking/routes/health.js
+++ activity-tracking/routes/health.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 // GET: Health
 router.get('/', async (req, res) => {
-    // TODO: Implement health endpoint
-});
+    const health = {
+        uptime: process.uptime(),
+        status: 'OK',
+        timestamp: Date.now()
+    };
+    try {
+        res.send(health);
+    } catch (error) {
+        health.message = error;
+        res.status(503).send();
+    }
+  });
   
 // GET: Readiness
 router.get('/ready', async (req, res) => {
-    // TODO: Implement readiness endpoint
+    const health = {
+        uptime: process.uptime(),
+        status: 'OK',
+        timestamp: Date.now()
+    };
+
+    const dependencies = [];
+
+    // Get connection status of mongoose
+    dependencies.push({
+        name: 'Mongoose',
+        status: mongoose.STATES[mongoose.connection.readyState],
+        connString: mongoose.connection.connString, 
+    });
+    const readiness = { ...health, dependencies };
+
+    // See: https://mongoosejs.com/docs/api/connection.html#Connection.prototype.readyState
+    if (mongoose.connection.readyState !== mongoose.STATES['connected']) {
+        // If mongoose dependency does not work, return 424 (Dependency Failed) response code
+        readiness.status = 'FAIL';
+        res.status(424).send(readiness);
+    }
+
+    try {
+        res.send(readiness);
+    } catch (error) {
+        readiness.message = error;
+        res.status(503).send();
+    }
 });
   
 module.exports = router;
