1f5706767b14d42c417e4ce1ca12918cae1e9462
Rehab Emad Eldeen <Rehab-Emad@users.noreply.github.com>
2024-01-12 20:21:20 +0000

    Add a failed test case to make the build of the CI fail

--- authservice/src/test/java/com/authservice/auth/AuthserviceApplicationTests.java
+++ authservice/src/test/java/com/authservice/auth/AuthserviceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.authservice.auth;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.authservice.auth.model.User;
 
 @SpringBootTest
 class AuthserviceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void setterAndGetterTests() {
+		User user = new User();
+		user.setUsername("test");
+		user.setPassword("password");
+		System.out.println("Testing that the user name is valid");
+		Assertions.assertEquals(user.getUsername(), "test2");
 	}
-
 }
